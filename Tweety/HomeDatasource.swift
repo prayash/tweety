081ce04be912e3a2e10f32069f756a27b03645eb
Prayash Thapa <prayasht@gmail.com>
2017-02-12 23:05:40 -0700

    configure navigation bar + items

--- Tweety/HomeDatasource.swift
+++ Tweety/HomeDatasource.swift
@@ -12,7 +12,7 @@ class HomeDatasource: Datasource {
     
     let users: [User] = {
         let prayashUser = User(name: "Prayash Thapa", username: "@effulgence", bioText: "I make music and write code that draws things. I am also an art and guitar nerd! Follow me if you're cool. I'll follow back! Promise!", profileImage: #imageLiteral(resourceName: "profile_image"))
-        let rayUser = User(name: "Ray Wenderlich", username: "@rwenderlich", bioText: "Ray Renderlich is an iPhone developer and tweets on topics related to iPhone, software and gaming. Check out our conference.", profileImage: #imageLiteral(resourceName: "profile_image"))
+        let rayUser = User(name: "Ray Wenderlich", username: "@rwenderlich", bioText: "Ray Renderlich is an iPhone developer and tweets on topics related to iPhone, software and gaming. Check out our conference.", profileImage: #imageLiteral(resourceName: "ray_profile_image"))
         
         let pakUser = User(name: "Pakalu Papito", username: "@loser", bioText: "Ray Renderlich is an iPhone developer and tweets on topics related to iPhone, software and gaming. Check out our conference Ray Renderlich is an iPhone developer and tweets on topics related to iPhone, software and gaming. Check out our conference.", profileImage: #imageLiteral(resourceName: "profile_image"))
         
--- Tweety/HomeDatasourceController.swift
+++ Tweety/HomeDatasourceController.swift
@@ -12,22 +12,28 @@ class HomeDatasourceController: DatasourceController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBarItems();
         
         let homeDatasource = HomeDatasource()
         self.datasource = homeDatasource
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
         return 0
     }
     
     override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
         if let user = self.datasource?.item(indexPath) as? User {
-            // Estimation of height of cell based on user's bioText
+            
+            // Estimation of the height of our cell based on user.bioText
+            
             let approximateWidthOfBioTextView = view.frame.width - 12 - 50 - 12 - 2
             let size = CGSize(width: approximateWidthOfBioTextView, height: 1000)
             let attributes = [NSFontAttributeName: UIFont.systemFont(ofSize: 15)]
+            
             let estimatedFrame = NSString(string: user.bioText).boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: attributes, context: nil)
+            
             return CGSize(width: view.frame.width, height: estimatedFrame.height + 66)
         }
         
@@ -41,4 +47,5 @@ class HomeDatasourceController: DatasourceController {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 50)
     }
+    
 }
